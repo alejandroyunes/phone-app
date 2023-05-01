ea5d517047a924556b3f479b874e2af4488134eb
Alejandro Yunes <alejandro.yunes@inxeption.com>
2023-04-30 23:05:16 -0500

    added header ui

--- phonebook-app/src/App.js
+++ phonebook-app/src/App.js
@@ -1,4 +1,5 @@
 import Layout from "./components/layout";
+import './app.scss'
 import PhoneBookHeader from "./components/phonebook-header";
 
 function PhoneApp() {
--- phonebook-app/src/components/phonebook-header/index.js
+++ phonebook-app/src/components/phonebook-header/index.js
@@ -1,14 +1,28 @@
 import './phonebook-header.scss'
 import {AiFillHome} from 'react-icons/ai'
+import {BsPlusCircleFill} from 'react-icons/bs'
+import {BiSearchAlt2} from 'react-icons/bi'
+
 export default function PhoneBookHeader (){
+
+    const handleSearchInput = (e) => {
+        const value = e.target.value
+
+    }
+
     return (
         <div className="header-wrapper">
             <div className='header-icons'>
-                <div>home</div>
                 <div><AiFillHome/></div>
+                <div><BsPlusCircleFill /></div>
             </div>
-            <div className='contact'></div>
-            <div className='search'></div>
+            <div className='contact'>
+                <h1>Contacts</h1>
+            </div>
+            <div className='search'>
+                <BiSearchAlt2/>
+                <input type='text' placeholder='search contact' onChange={(e)=> handleSearchInput(e)}/>
+            </div>
         </div>
     )
 }
